59a9a287e22f1874eeb2b9e577314fd1047035f5
motokikasai <kmomoky@hotmail.com>
2020-03-02 14:45:01 +0100

    Initial commit with general settings with form and bootstrap

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+// The following line makes sure your styles are included in the project. Don't remove this.
+import "../styles/main.scss";
+import "regenerator-runtime/runtime";
+import "bootstrap/scss/bootstrap.scss";
+// Import any additional modules you want to include below \/
+
+// \/ All of your javascript should go here \/
+
+const baseURL = "https://api.cryptonator.com/api/ticker/btc-usd";
